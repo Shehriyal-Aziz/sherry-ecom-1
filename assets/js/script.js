70af5a776f723a94f0a43fc7fc1e5930291dfeba
sheriyal <sheriyalaziz38@gmail.com>
2025-08-09 13:14:43 +0500

    1

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,29 @@ if(close){
         nav.classList.remove('active');
     });
 }
+
+// Elements
+const cartSidebar = document.getElementById("cart-sidebar");
+const cartOverlay = document.getElementById("cart-overlay");
+const openCartBtns = document.querySelectorAll("#lg-bag a, #mobile svg");
+const closeCartBtn = document.getElementById("close-cart");
+
+// Open Cart
+openCartBtns.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        e.preventDefault();
+        cartSidebar.classList.add("active");
+        cartOverlay.classList.add("active");
+    });
+});
+
+// Close Cart
+closeCartBtn.addEventListener("click", () => {
+    cartSidebar.classList.remove("active");
+    cartOverlay.classList.remove("active");
+});
+
+cartOverlay.addEventListener("click", () => {
+    cartSidebar.classList.remove("active");
+    cartOverlay.classList.remove("active");
+});
